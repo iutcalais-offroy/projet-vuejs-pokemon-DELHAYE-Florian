6993f2b39a501c7c9c048cf35449fd9753f0a1d1
DELHAYE Florian <delhaye2005@gmail.com>
2025-03-15 12:23:40 +0100

    Ajout de la connexion et de l'inscription

--- src/components/LoginForm.component.vue
+++ src/components/LoginForm.component.vue
@@ -9,8 +9,10 @@
           <n-form-item label="Mot de passe">
             <n-input type="password" v-model:value="loginPassword" placeholder="Entrez votre mot de passe" />
           </n-form-item>
-          <n-button type="primary">Se connecter</n-button>
+          <n-button type="primary" @click="handleLogin">Se connecter</n-button>
           <p>Pas de compte ? <a @click="activeTab = 'register'">S'inscrire</a></p>
+          <p v-if="userStore.errorMessage" class="error">{{ userStore.errorMessage }}</p>
+          <p v-if="userStore.successMessage" class="success">{{ userStore.successMessage }}</p>
         </n-form>
       </n-tab-pane>
       <n-tab-pane name="register" tab="Inscription">
@@ -24,8 +26,10 @@
           <n-form-item label="Confirmer le mot de passe">
             <n-input type="password" v-model:value="registerPasswordConfirm" placeholder="Confirmez votre mot de passe" />
           </n-form-item>
-          <n-button type="primary">S'inscrire</n-button>
+          <n-button type="primary" @click="handleRegister">S'inscrire</n-button>
           <p>Déjà inscrit ? <a @click="activeTab = 'login'">Se connecter</a></p>
+          <p v-if="userStore.errorMessage" class="error">{{ userStore.errorMessage }}</p>
+          <p v-if="userStore.successMessage" class="success">{{ userStore.successMessage }}</p>
         </n-form>
       </n-tab-pane>
     </n-tabs>
@@ -35,6 +39,7 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 import { NCard, NTabs, NTabPane, NForm, NFormItem, NInput, NButton } from 'naive-ui';
+import { useUserStore } from '../stores/userStore';
 
 const activeTab = ref('login');
 const loginEmail = ref('');
@@ -42,11 +47,34 @@ const loginPassword = ref('');
 const registerEmail = ref('');
 const registerPassword = ref('');
 const registerPasswordConfirm = ref('');
+const userStore = useUserStore();
+
+const handleRegister = async () => {
+  if (registerPassword.value !== registerPasswordConfirm.value) {
+    userStore.errorMessage = 'Les mots de passe ne correspondent pas.';
+  } else {
+    console.log('Register Email:', registerEmail.value);
+    console.log('Register Password:', registerPassword.value);
+    await userStore.register(registerEmail.value, registerPassword.value);
+  }
+};
+
+const handleLogin = async () => {
+  console.log('Login Email:', loginEmail.value);
+  console.log('Login Password:', loginPassword.value);
+  await userStore.login(loginEmail.value, loginPassword.value);
+};
 </script>
 
 <style scoped>
 .login-container, .login, .register {
     display: flex;
     flex-direction: column;
+}
+.error {
+    color: red;
+}
+.success {
+    color: green;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import {createApp} from 'vue';
-import {createPinia} from 'pinia';
-import NaiveUI from 'naive-ui';
+import { createApp } from 'vue';
 import App from './App.vue';
-import {router} from './router';
+import { createPinia } from 'pinia';
+import NaiveUI from 'naive-ui';
+import { router } from './router';
 
-const pinia = createPinia();
 const app = createApp(App);
+const pinia = createPinia();
 
 app.use(NaiveUI);
 app.use(pinia);
-app.use(router)
+app.use(router);
 app.mount('#app');
